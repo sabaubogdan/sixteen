9c95464cc47db823c84161707f6319c11c3ca628
Bogdan Sabau <sabaubogdanvasile@gmail.com>
2018-01-31 20:56:09 +0200

    Initial commit

---
+++ src/test/java/xyz/vegaone/sixteen/SixteenApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.vegaone.sixteen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SixteenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
